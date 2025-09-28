c858b9b5ad37c19c1ba45c9312ea0480a84e8026
Philip Shaji Baby <philipshajibaby@gmail.com>
2025-09-28 11:02:02 -0400

    Everything is wokring before deplying backend

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from openai import OpenAI
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 app = FastAPI()
